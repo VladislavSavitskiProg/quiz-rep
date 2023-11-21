1cf85d889a16a62cdec84fb4c8348c1b64412445
Vladislav Savitski <vladislav.savitski.prog@gmail.com>
2023-11-21 14:16:32 +0300

    add simple view

--- src/Controller/QuizController.php
+++ src/Controller/QuizController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Questions;
+use App\Repository\AnswersRepository;
+use App\Repository\QuestionsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class QuizController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Environment $twig, QuestionsRepository $questionsRepository): Response
     {
-        return $this->render('quiz/index.html.twig', [
-            'controller_name' => 'QuizController',
-        ]);
+        return new Response($twig->render('quiz/index.html.twig', [
+            'questions' => $questionsRepository->findAll(),
+        ]));
+    }
+
+    #[Route('/question/{id}', name: 'question')]
+    public function show(Environment $twig, Questions $question, AnswersRepository $answersRepository): Response
+    {
+        return new Response($twig->render('quiz/show.html.twig', [
+            'question' => $question,
+            'answers' => $answersRepository->findBy(['questions' => $question]),
+        ]));
     }
 }
